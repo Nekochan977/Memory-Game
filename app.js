da8171b703a137496aac1437c82025e88382acb8
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-08-09 15:39:38 +0200

    Add logic on the clicked cards

--- app.js
+++ app.js
@@ -142,6 +142,7 @@ const randomize = () => {
 //Card generator function
 const cardGenerator = () => {
     const cardData = randomize()
+    const cards = document.querySelectorAll('.card')
     cardData.forEach((item) =>{
 
         //Generate the html
@@ -157,11 +158,45 @@ const cardGenerator = () => {
         face.alt = item.name
         back.src = backImage.img
         back.alt = backImage.name
+        card.setAttribute('name', item.name)
 
         //Attach the cards to the section
         section.appendChild(card)
         card.appendChild(face)
         card.appendChild(back)
+
+        //Add event on card
+        card.addEventListener('click', (e) => {
+            card.classList.toggle('toggleCard')
+            checkCard(e)
+        })
     })
+}
+
+//Check card
+
+const checkCard = (e) => {
+    const clickedCard = e.target
+    clickedCard.classList.add('flipped')
+    const flippedCards = document.querySelectorAll('.flipped')
+
+    // console.log(clickedCard)
+    // Add logic
+    if(flippedCards.length === 2){
+        if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')){
+            console.log("It's a Match")
+            //If match remove possibility to click on the card
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped")
+                card.style.pointerEvents = 'none'
+            })
+        } else {
+            console.log("Wrong")
+            flippedCards.forEach((card)=>{
+                card.classList.remove("flipped")
+                setTimeout(() => card.classList.remove("toggleCard"), 1000)
+            })
+        }
+    }
 }
 cardGenerator()
